3533977feecc4658cb91363d3401831caa08b0de
lulumuts <lulumutuli24@gmail.com>
2020-10-31 11:59:37 +0000

    initial commit

---
+++ tropic/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <api />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import api from "./components/api.vue";
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    api,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tropic/src/components/api.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>TO DO'S</h1>
+    <div v-for="item in todos" :key="item.id">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "api",
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    this.allData();
+  },
+  methods: {
+    async allData() {
+      await this.axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => {
+          this.todos = response.data;
+          console.log(response.data);
+        });
+    },
+  },
+};
+</script>
