a87bdf5dc86ec222151322bd7d96c7257473b45a
lulumuts <lulumutuli24@gmail.com>
2020-10-31 17:06:45 +0000

    filter by completed functionality implemented

--- tropic/src/components/api.vue
+++ tropic/src/components/api.vue
@@ -2,9 +2,28 @@
   <div>
     <h1>{{ msg }}</h1>
     <input v-model="search" />
-    <div v-for="item in searchFilter" :key="item.id">
-      {{ item }}
-    </div>
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">{{ checked }}</label>
+
+    <ul class="flex-container wrap">
+      <div v-for="item in filterTodos" :key="item.id">
+        <div class="item">
+          <!-- {{ item }} -->
+          <ul>
+            <li>userId: {{ item.id }}</li>
+            <li>title: {{ item.title }}</li>
+            <li>Completed: {{ item.completed }}</li>
+          </ul>
+        </div>
+      </div>
+    </ul>
+    <!-- <div class="container">
+      <div class="flex-container">
+        <div v-for="item in filterTodos" :key="item.id">
+          <div>{{ item }}</div>
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -16,13 +35,12 @@ export default {
     return {
       todos: [],
       search: "",
+      checked: false,
     };
   },
   computed: {
-    searchFilter() {
-      return this.todos.filter((x) => {
-        return x.title.toLowerCase().includes(this.search.toLowerCase());
-      });
+    filterTodos: function() {
+      return this.filterBySearch();
     },
   },
   mounted() {
@@ -39,6 +57,53 @@ export default {
         console.log(error);
       }
     },
+    filterBySearch() {
+      return this.todos.filter((x) => {
+        return (
+          x.title.toLowerCase().includes(this.search.toLowerCase()) &&
+          x.completed === this.checked
+        );
+      });
+    },
+    filteredCompleted(todos) {
+      //   return todos.filter((todo) => todo.completed == this.checked);
+      todos.filter((x) => {
+        if (x.completed === this.checked) {
+          return x;
+        }
+      });
+    },
   },
 };
 </script>
+<style scoped>
+.highlight {
+  background-color: yellow;
+}
+
+.flex-container {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+
+  -ms-box-orient: horizontal;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -moz-flex;
+  display: -webkit-flex;
+  display: flex;
+}
+.wrap {
+  -webkit-flex-wrap: wrap;
+  flex-wrap: wrap;
+}
+.item {
+  height: 150px;
+  width: 300px;
+  margin: 20%;
+}
+ul li {
+  list-style: none;
+}
+</style>
